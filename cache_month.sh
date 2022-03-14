a8da40bfb9786b5f2bf6619676b6d7e490551ed8
Glenn K. Lockwood <glock@lbl.gov>
2022-03-14 12:01:37 -0700

    add optional csv or json

--- cache_month.sh
+++ cache_month.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 MONTH=$1 # eg 2020-02
+FORMAT="${2:-json}"
 THIS_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 CACHE_ISDCT=${CACHE_ISDCT:-"${THIS_DIR}/anonymize_isdct.py"}
@@ -20,6 +21,9 @@ for inputf in $(find ${DATA_DIR}/${MONTH}-* -name Intel\*.tgz)
 do
     # inputf = /global/homes/g/glock/h5lmt/2020-02-25/Intel_DCT_20200225.tgz
     bn=$(basename "$inputf" .tgz)
-    $CACHE_ISDCT -j "$inputf" -o "${bn}.json" && gzip -9 "${bn}.json"
-    $CACHE_ISDCT -c "$inputf" -o "${bn}.csv" && gzip -9 "${bn}.csv"
+    if [ $FORMAT == "json" ]; then
+        $CACHE_ISDCT -j "$inputf" -o "${bn}.json" && gzip -9 "${bn}.json"
+    else
+        $CACHE_ISDCT -c "$inputf" -o "${bn}.csv" && gzip -9 "${bn}.csv"
+    fi
 done
